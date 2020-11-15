4fe6f5bfcb7112c1e6aa5fae51c5a0c089f261c4
Andrey Sprynga <imolo4nik@gmail.com>
2020-11-16 03:36:56 +0700

    m1872: vendor_prop.mk -> vendor.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Releasetools
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
